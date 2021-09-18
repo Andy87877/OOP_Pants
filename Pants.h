3e3f7067eb485270fe63ab5dcee1c0f2d37556d1
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-09-18 10:02:37 +0800

    Create Pants.h

---
+++ Pants.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Pants{
+        string color;
+        int size;
+    public:
+        void Pants(string, int);
+        
+        
+        string ans_Color (void) {return (color);}
+        int ans_Size (void) {return (size);}     
+};
